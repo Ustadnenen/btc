fb44f01dadc504ec1624bc3bcb7ee9e0d872096e
Ustadnenen <61479817+Ustadnenen@users.noreply.github.com>
2020-02-26 05:32:28 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+
+$email = "pompadennycfd@gmail.com";
+$address = "12GwXxkGMKBDhKe5N7tX7mUzTtJaVnz3jY";
+
+/* Link Pass :    https://semawur.com/0dGrBL7wm0   */
+
+$pass_scrip = "2020new";
+
+
+//https://www.hachwaclaims.xyz/?r=15856
+
