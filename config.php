174c0e4944d6063b4003731d0911e0660d741b8f
Ustadnenen <61479817+Ustadnenen@users.noreply.github.com>
2020-02-26 13:31:39 +0000

    Update config.php

--- config.php
+++ config.php
@@ -5,7 +5,7 @@ $address = "12GwXxkGMKBDhKe5N7tX7mUzTtJaVnz3jY";
 
 /* Link Pass :    https://semawur.com/0dGrBL7wm0   */
 
-$pass_scrip = "2020new";
+$pass_scrip = "blocks";
 
 
 //https://www.hachwaclaims.xyz/?r=15856
