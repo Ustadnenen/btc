cfe133516f23ebbb9f43a136bf7dc793a5eb3eca
Ustadnenen <61479817+Ustadnenen@users.noreply.github.com>
2020-03-01 12:59:00 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,11 +1,11 @@
 <?php
 
 $email = "pompadennycfd@gmail.com";
-$address = "12GwXxkGMKBDhKe5N7tX7mUzTtJaVnz3jY";
+$address = "1B2br9TmDE55rSDKypifZjDih7gcBM2PV3";
 
 /* Link Pass :    https://semawur.com/0dGrBL7wm0   */
 
-$pass_scrip = "blocks";
+$pass_scrip = "hhhskej";
 
 
 //https://www.hachwaclaims.xyz/?r=15856
